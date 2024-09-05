0f0fb161702b9166b98b16c26892b0fe458e83a0
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-09-05 19:08:21 +0600

    update 3

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    SERVER_URL: "http://localhost:8080",
-    // SERVER_URL: "https://bussiness-api.onrender.com",
+    // SERVER_URL: "http://localhost:8080",
+    SERVER_URL: "https://bussiness-api.onrender.com",
   },
 };
 
