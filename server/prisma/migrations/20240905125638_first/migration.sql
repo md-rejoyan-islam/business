e171f91bac72c55c07e6ab049fd21d76549289c5
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-09-05 19:04:49 +0600

    update 2

--- server/prisma/migrations/20240905125638_first/migration.sql
+++ server/prisma/migrations/20240905125638_first/migration.sql
@@ -196,6 +196,32 @@ CREATE TABLE `DailyCash` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `DailyOthersCost` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `amount` DOUBLE NOT NULL,
+    `date` VARCHAR(191) NOT NULL,
+    `createAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updateAt` DATETIME(3) NOT NULL,
+    `dailyCashId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `CustomerCheck` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `amount` DOUBLE NOT NULL,
+    `date` VARCHAR(191) NOT NULL,
+    `bank` VARCHAR(191) NOT NULL,
+    `customerId` INTEGER NOT NULL,
+    `createAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updateAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `GrayChalan` ADD CONSTRAINT `GrayChalan_grayId_fkey` FOREIGN KEY (`grayId`) REFERENCES `Gray`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -252,3 +278,9 @@ ALTER TABLE `CustomerPayment` ADD CONSTRAINT `CustomerPayment_customerId_fkey` F
 
 -- AddForeignKey
 ALTER TABLE `CustomerPayment` ADD CONSTRAINT `CustomerPayment_customerChalanId_fkey` FOREIGN KEY (`customerChalanId`) REFERENCES `CustomerChalan`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `DailyOthersCost` ADD CONSTRAINT `DailyOthersCost_dailyCashId_fkey` FOREIGN KEY (`dailyCashId`) REFERENCES `DailyCash`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `CustomerCheck` ADD CONSTRAINT `CustomerCheck_customerId_fkey` FOREIGN KEY (`customerId`) REFERENCES `Customer`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
